dfc006d7fc6a23e4d5031bb71d3febb014653d00
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2022-11-17 01:27:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,6 +169,7 @@ def main():
 
     URL = 'https://azureresoucenamer.streamlit.app/'
     PRJ = 'https://github.com/users/SurendraRedd/projects/4'
+    DIS = 'https://github.com/SurendraRedd/azureresoucenamer/discussions'
     with st.expander('Share This Tool'):
         st.write(URL)
         st.markdown(f'''
@@ -177,6 +178,7 @@ def main():
                 unsafe_allow_html=True)
     with st.expander('Raise an Issue'):
         st.write(PRJ)
+        st.write(DIS)
 
 
 # main function call
